72262702c30b5446f4578bbd95684f43983d8e2c
Vladislav Ivanov <JokeIvanov@yandex.ru>
2021-08-26 10:44:51 +0300

    Add Bounce effect to right and wrong answer

--- Assets/Script/ClickElement.cs
+++ Assets/Script/ClickElement.cs
@@ -1,6 +1,8 @@
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
+using System.Collections;
 
 public class ClickElement : MonoBehaviour
 {
@@ -14,14 +16,31 @@ public class ClickElement : MonoBehaviour
 
     private void TaskOnClick()
     {
-        if (TryGetComponent(out RightAnswer rightAnswer))
+        StartCoroutine(ShakeAndBounceCoroutine());
+    }
+
+    IEnumerator ShakeAndBounceCoroutine()
+    {
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+        TryGetComponent(out RightAnswer rightAnswer);
+        if (rightAnswer != null)
         {
-            CorrectAnswer?.Invoke();
+            transform.DOShakeScale(0.5f, strength: new Vector3(0.1f * Mathf.Sign(this.transform.position.x), 0.1f * Mathf.Sign(this.transform.position.y), 0), vibrato: 0, randomness: 0, fadeOut: false);
         }
         else
         {
-            Debug.Log(false);
+            transform.DOShakePosition(1.5f, strength: new Vector3(1.5f, 0, 0), vibrato: 5, randomness: 1, snapping: false, fadeOut: true);
         }
+
+        if (rightAnswer != null)
+        {
+            yield return new WaitForSeconds(0.8f);
+            CorrectAnswer?.Invoke();
+        }
+        else
+            yield return new WaitForSeconds(1.5f);
+
+        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 }
 
